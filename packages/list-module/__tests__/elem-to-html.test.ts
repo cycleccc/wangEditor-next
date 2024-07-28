465382e29eb5553f94cd2a96611a79d1354f8c84
yaolongfei <2991205548@qq.com>
2024-07-28 22:44:26 +0800

    test(list module): add complex list test

--- packages/list-module/__tests__/elem-to-html.test.ts
+++ packages/list-module/__tests__/elem-to-html.test.ts
@@ -5,6 +5,7 @@
 
 import createEditor from '../../../tests/utils/create-editor'
 import { ELEM_TO_EDITOR } from '../src/utils/maps'
+import $, { getTagName } from '../src/utils/dom'
 import listItemToHtmlConf from '../src/module/elem-to-html'
 
 describe('module elem-to-html', () => {
@@ -77,5 +78,45 @@ describe('module elem-to-html', () => {
       prefix: '<ul>',
       suffix: '</ul></ul>',
     })
+  })
+
+  // empty item
+  test('should return empty string for empty Dom7Array', () => {
+    // 创建一个空的 Dom7Array
+    const $elem = $()
+    const tagName = getTagName($elem)
+    expect(tagName).toBe('')
+  })
+})
+
+describe('module elem-to-html complex list', () => {
+  const unOrderedElem1 = { type: 'list-item', ordered: false, children: [{ text: '' }] }
+  const unOrderedElem2 = { type: 'list-item', ordered: false, level: 1, children: [{ text: '' }] }
+  const unOrderedElem3 = { type: 'list-item', ordered: false, children: [{ text: '' }] }
+  const orderedElem1 = { type: 'list-item', ordered: true, level: 1, children: [{ text: '' }] }
+  const orderedElem2 = { type: 'list-item', ordered: true, children: [{ text: '' }] }
+  const firstTextHtml = { type: 'paragraph', children: [{ text: 'hello' }] }
+  const lastTextHtml = { type: 'paragraph', children: [{ text: 'world' }] }
+
+  const editor = createEditor({
+    content: [
+      firstTextHtml,
+      unOrderedElem1,
+      unOrderedElem2,
+      unOrderedElem3,
+      orderedElem1,
+      orderedElem2,
+      lastTextHtml,
+    ],
+  })
+
+  // elem 绑定 editor
+  ELEM_TO_EDITOR.set(unOrderedElem1, editor)
+
+  test('complex list', () => {
+    const html = editor.getHtml()
+    expect(html).toEqual(
+      '<p>hello</p><ul><li></li><ul><li></li></ul><li></li><ol><li></li></ol></ul><ol><li></li></ol><p>world</p>'
+    )
   })
 })
