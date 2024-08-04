53363470cb7d60aa25357ccf4356ac0c823dcb26
yaolongfei <2991205548@qq.com>
2024-08-04 21:52:16 +0800

    test(image module): improve image tests
    improve image helper and plugin tests

--- packages/basic-modules/__tests__/image/plugin.test.ts
+++ packages/basic-modules/__tests__/image/plugin.test.ts
@@ -9,12 +9,15 @@ import withImage from '../../src/modules/image/plugin'
 describe('image plugin', () => {
   const editor = withImage(createEditor())
   const elem = { type: 'image', children: [{ text: '' }] }
+  const pargraph = { type: 'paragraph', children: [{ text: '' }] }
 
   it('image is inline', () => {
     expect(editor.isInline(elem)).toBeTruthy()
+    expect(editor.isInline(pargraph)).toBeFalsy()
   })
 
   it('image is void', () => {
     expect(editor.isVoid(elem)).toBeTruthy()
+    expect(editor.isVoid(pargraph)).toBeFalsy()
   })
 })
