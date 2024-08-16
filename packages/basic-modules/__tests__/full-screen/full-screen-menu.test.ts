0227cb7c67847ec32f5af96899e2182953c85d10
cycleccc <2991205548@qq.com>
2024-08-16 22:23:00 +0800

    test(full screen): add get icon and get title test

--- packages/basic-modules/__tests__/full-screen/full-screen-menu.test.ts
+++ packages/basic-modules/__tests__/full-screen/full-screen-menu.test.ts
@@ -4,11 +4,21 @@
  */
 
 import createEditor from '../../../../tests/utils/create-editor'
+import { CANCEL_FULL_SCREEN_SVG, FULL_SCREEN_SVG } from '../../src/constants/icon-svg'
 import FullScreen from '../../src/modules/full-screen/menu/FullScreen'
+import { t } from '@wangeditor-next/core'
 
 describe('full screen menu', () => {
   const editor = createEditor()
   const menu = new FullScreen()
+
+  it('get value', () => {
+    expect(menu.getValue(editor)).toBe('')
+  })
+
+  it('is disabled', () => {
+    expect(menu.isDisabled(editor)).toBeFalsy()
+  })
 
   it('full screen menu', done => {
     menu.exec(editor, '') // 设置全屏
@@ -17,6 +27,23 @@ describe('full screen menu', () => {
     menu.exec(editor, '') // 取消全屏（有延迟）
     setTimeout(() => {
       expect(menu.isActive(editor)).toBeFalsy()
+      done()
+    }, 500)
+  })
+
+  it('get title', () => {
+    expect(menu.getTitle(editor)).toBe(t('fullScreen.cancelTitle'))
+    menu.exec(editor, '')
+    expect(menu.getTitle(editor)).toBe(t('fullScreen.title'))
+  })
+
+  it('get icon', done => {
+    let svg = menu.getIcon(editor)
+    expect(svg).toBe(FULL_SCREEN_SVG)
+    menu.exec(editor, '')
+    setTimeout(() => {
+      svg = menu.getIcon(editor)
+      expect(svg).toBe(CANCEL_FULL_SCREEN_SVG)
       done()
     }, 500)
   })
