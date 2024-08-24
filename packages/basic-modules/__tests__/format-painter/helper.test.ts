0becd1e8b473b58478b5a97d65b712b2a94ca97b
CodePencil <jiawei.wang2021@gmail.com>
2024-08-24 18:05:16 +0800

    test(format painter): add more tests

---
+++ packages/basic-modules/__tests__/format-painter/helper.test.ts
@@ -0,0 +1,36 @@
+/**
+ * @description format painter helper test
+ * @author CodePencil
+ */
+
+import { Editor } from 'slate'
+import createEditor from '../../../../tests/utils/create-editor'
+import { clearAllMarks } from '../../src/modules/format-painter/helper'
+
+describe('format painter helper', () => {
+  let editor: any
+  let startLocation: any
+
+  beforeEach(() => {
+    editor = createEditor()
+    startLocation = Editor.start(editor, [])
+  })
+
+  afterEach(() => {
+    editor = null
+    startLocation = null
+  })
+
+  it('clear all marks ', () => {
+    editor.select(startLocation)
+
+    editor.addMark('bold', true)
+    editor.addMark('italic', true)
+
+    expect(Editor.marks(editor)).toEqual({ bold: true, italic: true })
+
+    clearAllMarks(editor)
+
+    expect(Editor.marks(editor)).toEqual({})
+  })
+})
