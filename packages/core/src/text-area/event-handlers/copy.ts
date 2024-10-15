547c11c0e82cb26da49f96f2b11013bec59d8f91
yaolongfei <2991205548@qq.com>
2024-10-15 11:30:03 +0800

    style: refactor by eslint

--- packages/core/src/text-area/event-handlers/copy.ts
+++ packages/core/src/text-area/event-handlers/copy.ts
@@ -4,18 +4,18 @@
  */
 
 import { IDomEditor } from '../../editor/interface'
-// import { DomEditor } from '../../editor/dom-editor'
-import TextArea from '../TextArea'
 import { hasEditableTarget } from '../helpers'
+import TextArea from '../TextArea'
 
-function handleOnCopy(e: Event, textarea: TextArea, editor: IDomEditor) {
+function handleOnCopy(e: Event, _textarea: TextArea, editor: IDomEditor) {
   const event = e as ClipboardEvent
 
-  if (!hasEditableTarget(editor, event.target)) return
+  if (!hasEditableTarget(editor, event.target)) { return }
   event.preventDefault()
 
   const data = event.clipboardData
-  if (data == null) return
+
+  if (data == null) { return }
   editor.setFragmentData(data)
 }
 
